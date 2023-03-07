08e49d8467cada674bff4c046142554a62472b39
Nethra1218 <126752832+Nethra1218@users.noreply.github.com>
2023-03-07 16:11:54 +0530

    Operators

---
+++ Operators.py
@@ -0,0 +1,46 @@
+#Arithematic operator
+#Addition
+a=5
+b=6
+print(a+b)
+
+#substraction
+a=10
+b=5
+print(a-b)
+
+#multiplication
+a=20
+b=10
+print(a*b)
+
+#division
+a=10
+b=10
+print(a/b)
+
+#modulus
+a=5
+b=2
+print(a%b)
+
+#Exponential
+a=2
+b=3
+print(a**b)
+
+#floor division
+a=20
+b=5
+print(a//b)
+
+#Assignment operator
+# type function
+x=20
+print(type(x))
+
+a=5
+a+=3  # +=operator
+a-=2  # -=operator
+print(a)
+
